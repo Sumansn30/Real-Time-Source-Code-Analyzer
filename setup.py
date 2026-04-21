b96692c4841a335b320c80b5243c7721384987a1
Sumansn30 <sumansn721@gmail.com>
2026-04-21 14:19:23 +0530

    setupfile done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+
+
+setup(
+    name="realtime-source-code-analyzer",
+    version="0.1.0",
+    author="Suman",
+    author_email="sumansn721@gmail.com",
+    packages= find_packages(),
+    install_requires=[] 
+)
